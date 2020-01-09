e2d04a7d33d186a879dbe057d561b98e53e0d48a
Nate <nate@Nates-MacBook-Air.local>
2020-01-09 14:00:01 -0600

    Added an example function that uses an int array as an argument to a c function

--- src/test.cpp
+++ src/test.cpp
@@ -6,6 +6,17 @@ extern "C"{
 		printf("%s", str);
 		return strlen(str);
 	}
+
+	int csum(int* array, int length)
+	{
+		int sum = 0;
+		for(int i = 0; i < length; i++)
+		{
+			sum += array[i];
+		}
+		array[length-1] = 0;
+		return sum;
+	}
 }
 
 // g++ -c -fPIC foo.cpp -o foo.o
--- src/test.py
+++ src/test.py
@@ -1,6 +1,20 @@
 
-from ctypes import cdll
-lib = cdll.LoadLibrary('libtest.so')
+import ctypes as ct
+
+lib = ct.cdll.LoadLibrary('libtest.so')
 
 ret = lib.cprint(b"testing!\n")
+
+print(lib.cprint.argtypes)
 print("returned: {}".format(ret))
+
+sum_list = [1, 2, 3]
+
+
+c_list = (ct.c_int * len(sum_list))(*sum_list)
+for i in range(0, len(c_list)):
+	print(c_list[i])
+
+
+print(lib.csum(c_list, len(c_list)))
+print(c_list[-1])
